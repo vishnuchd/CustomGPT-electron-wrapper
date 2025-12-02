03a127e6eb019eb7b589fbc8b5b3b44fb528ba55
Vishnu Sharma <vishnudbimcs@gmail.com>
2025-12-02 18:14:02 +0530

    miscellaneous updates

--- preload.js
+++ preload.js
@@ -5,6 +5,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   injectCSS: (css) => ipcRenderer.invoke('inject-css', css),
   executeJS: (code) => ipcRenderer.invoke('execute-js', code),
-  reloadCustomizations: () => ipcRenderer.invoke('reload-customizations')
+  reloadCustomizations: () => ipcRenderer.invoke('reload-customizations'),
+  // Auth0 methods
+  auth0Login: () => ipcRenderer.invoke('auth0-login'),
+  auth0GetUser: (accessToken) => ipcRenderer.invoke('auth0-get-user', accessToken)
 });
 
