906cb16bba661233005f8ec89fa1e94cc6ddd83b
Vishnu Sharma <vishnudbimcs@gmail.com>
2026-01-06 11:48:51 +0530

    removing redundant code

--- preload.js
+++ preload.js
@@ -1,9 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  injectCSS: (css) => ipcRenderer.invoke('inject-css', css),
-  executeJS: (code) => ipcRenderer.invoke('execute-js', code),
-  reloadCustomizations: () => ipcRenderer.invoke('reload-customizations'),
   // SendGrid email
   sendWebsiteEmail: (data) => ipcRenderer.invoke('send-website-email', data),
   sendSharepointEmail: (data) => ipcRenderer.invoke('send-sharepoint-email', data),
